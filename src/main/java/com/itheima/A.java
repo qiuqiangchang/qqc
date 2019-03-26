7f1d27ac235d203f2649be6e96fedb332e8afecc
qiuqiangchang <407975520@qq.com>
2019-03-26 11:08:20 +0800

    创建新的分支，查询全部

--- src/main/java/com/itheima/A.java
+++ src/main/java/com/itheima/A.java
@@ -4,5 +4,6 @@ public class A {
     public static void main(String[] args) {
         System.out.println("项目搭建");
         System.out.println("修改了部分源码");
+        System.out.println("创建新的分支，查询全部");
     }
 }
